7ac496b68b9efebf30ccec8f157922bb0d2df29c
Carl Taylor <carl@etaylor.me>
2023-06-07 19:20:46 -0700

    Add init script and jenkins repo isolation to mvn

--- bin/_init.sh
+++ bin/_init.sh
@@ -18,4 +18,14 @@ Darwin*)
 *) echo "Unsupported system: ${unameOut}" && exit 1 ;;
 esac
 
+# Maven 3 corrupts concurrent builds!
+# If EXECUTOR_NUMBER is defined, we are running on jenkins, so set the maven
+# repo to a local one. This is to avoid concurrent builds from overwriting each
+# other's artifacts, and requires --update-snapshots to get snapshots from the
+# remote repo.
+if [ -n "${EXECUTOR_NUMBER:-}" ]; then
+  mkdir -p "$HOME/.m2/repo@$EXECUTOR_NUMBER"
+  export MAVEN_OPTS="-Dmaven.repo.local=$HOME/.m2/repo@$EXECUTOR_NUMBER --update-snapshots --no-transfer-progress"
+fi
+
 export SED_COMMAND
